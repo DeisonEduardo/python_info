05772eaa5b287b71116d5ab82faf017631bda49f
DeisonEduardo <deisinneduardo@gmail.com>
2026-06-26 09:07:29 -0300

    Create questão07.py

---
+++ questão07.py
@@ -0,0 +1,9 @@
+Sobrenome = input(“Digite o sobrenome do apresentador: “)
+
+if sobrenome == “PINHEIRO” or sobrenome == “ARAÚJO”:
+   print(“Bom Dia Nação”)
+elif sobrenome == “BONNER” or sobrenome == “VASCONCELOS”:
+   print(“Jornal Brasileiro”)
+else:
+   print(“Apresentador(a) desconhecido(a).”)
+
